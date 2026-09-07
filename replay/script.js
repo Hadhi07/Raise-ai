7fb175ae0e27df563a8aaab8a8296397c7961a33
Hadhi07 <hadhi07@example.com>
2026-09-07 11:29:42 +0530

    Update webinar replay video link and add student testimonial video

--- replay/script.js
+++ replay/script.js
@@ -7,7 +7,7 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player('youtube-player', {
         height: '100%',
         width: '100%',
-        videoId: 'ex6abJ4R-qM', // Actual Video ID
+        videoId: '2LRR6rgiSUM', // Actual Video ID
         playerVars: {
             'autoplay': 1,
             'controls': 1, // Enable controls for zooming
--- replay/script3.js
+++ replay/script3.js
@@ -7,7 +7,7 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player('youtube-player', {
         height: '100%',
         width: '100%',
-        videoId: 'P4btOK7SO8w', // Actual Video ID
+        videoId: '2LRR6rgiSUM', // Actual Video ID
         playerVars: {
             'autoplay': 1,
             'controls': 1, // Enable controls for zooming
